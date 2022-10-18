16791ce20049bd7b7e91f43430a663729844294b
kenami0981 <amelia.janek.aj@gmail.com>
2022-10-18 21:41:19 +0200

    Part 2; Exercise 4 - Map Response to Model

---
+++ src/app/services/employee.response.ts
@@ -0,0 +1,7 @@
+export interface EmployeeResponse {
+  readonly id: string;
+  readonly employee_name: string;
+  readonly employee_age: number;
+  readonly employee_salary: string;
+  readonly profile_image: string;
+}
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,13 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import {map, Observable} from 'rxjs';
 import { PersonModel } from '../model/person.model';
 import { AddEmployeeModel } from '../model/add-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees'
+    ).pipe(map((response:ApiResponse<EmployeeResponse[]>)=>{
+      return response.data.map((employeeResponse:EmployeeResponse)=> {
+        return {
+          name: employeeResponse.employee_name,
+          age: employeeResponse.employee_age,
+          personalNumber: employeeResponse.id,
+          mail: employeeResponse.employee_name+"@lowgular.io",
+          img: employeeResponse.profile_image,
+          surname: ''
+        }
+      })
+    }))
   }
   createEmployee(employee: AddEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee);
